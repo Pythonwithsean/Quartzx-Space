107e2c3a95ec93c2b8d2be5635af238a562e4234
Sean <107402787+Pythonwithsean@users.noreply.github.com>
2023-10-17 18:59:29 +0100

    "Commit"

--- Server/Server.js
+++ Server/Server.js
@@ -4,15 +4,49 @@ const PORT = process.env.PORT || 3001; // Use PORT value from .env file or defau
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://pythonwithsean:Sophia%2320212@quartzx.ehghmhv.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected…");
+  })
+  .catch((err) => console.log(err));
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.post("/submit-form", (req, res) => {
-//   const data = req.body;
-//   console.log(data);
-//   console.log(data);
-// });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 app.post("/submit-form", (req, res) => {
   // Handle POST requests to /submit-form
