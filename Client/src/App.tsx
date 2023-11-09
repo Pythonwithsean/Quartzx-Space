619008737121f7abe0aacf3480aeba4fc1034878
Pythonwithsean <Lunafcs@Protonmail.com>
2023-11-09 19:02:21 +0100

    "Commit"

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,23 +1,30 @@
 import "./Styles/App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-
+import { v4 as uuidv4 } from "uuid";
 import TextEditor from "./Components/TextEditor";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/QuartzxSpace/*" element={<TextEditor />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/QuartzxSpace/:id" element={<TextEditor />} />
+        <Route
+          path="/QuartzxSpace/*"
+          element={<Navigate to={`/QuartzxSpace/${uuidv4()}`} />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
