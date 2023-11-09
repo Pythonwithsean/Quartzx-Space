f5b84df4eb92d7f44089bf244d63d1a777c5d26b
Pythonwithsean <Lunafcs@Protonmail.com>
2023-11-09 17:24:56 +0100

    "Commit"

--- Client/src/Pages/QuartzxSpace.tsx
+++ Client/src/Pages/QuartzxSpace.tsx
@@ -2,19 +2,28 @@ import { Link } from "react-router-dom";
 import Bar from "../Components/Bar";
 import TextEditor from "../Components/TextEditor";
 import "../Styles/QuartzxSpace.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function QuartzxSpace(): JSX.Element {
-  return (  
+  return (
     <>
       <Bar />
-      <div className="Wrapper">
-        {" "}
-        <Link to="/" className="Logo">
-          Quartzx Space
-        </Link>
-      </div>
-      <div className="Text-Area">
-        <TextEditor />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/documents/:id">
+            <div className="Wrapper">
+              {" "}
+              <Link to="/" className="Logo">
+                Quartzx Space
+              </Link>
+            </div>
+            <div className="Text-Area">
+              <TextEditor />
+            </div>
+          </Route>
+          R
+        </Routes>
+      </Router>
     </>
   );
 }
